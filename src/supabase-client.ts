ab1d26f1ac2a59858deb2e118e5ee0867b80a8a8
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-11-13 15:25:33 +0600

    integrated supabase

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import {createClient} from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_API_KEY
+)
+
